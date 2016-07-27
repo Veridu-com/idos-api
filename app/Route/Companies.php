fb06491c4579dda49e1f8059e6b050d103ee4000
Cauê Lorenzato <caue@veridu.com>
2016-07-27 10:53:26 -0300

    Copyright first. CS fixer.

--- app/Route/Companies.php
+++ app/Route/Companies.php
@@ -1,10 +1,11 @@
 <?php
 
-declare(strict_types=1);
 /*
  * Copyright (c) 2012-2016 Veridu Ltd <https://veridu.com>
  * All rights reserved.
  */
+
+declare(strict_types=1);
 
 namespace App\Route;
 
