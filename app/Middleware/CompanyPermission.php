be11752d21eafe9606d91780dccbf1113fe74c27
Rafael Mello Campanari <rafael@veridu.com>
2016-08-17 13:13:42 -0300

    CompanyPermission | Adding Self and Parent or Self permission

--- app/Middleware/CompanyPermission.php
+++ app/Middleware/CompanyPermission.php
@@ -20,11 +20,13 @@ use Psr\Http\Message\ServerRequestInterface;
  * Scope: Route.
  * This middleware is responsible to add a "allowed" parameter on the $response object.
  *
- * FIXME Remove Container injection!
+ * -FIXME Remove Container injection!
  */
 class CompanyPermission implements MiddlewareInterface {
-    const PUBLIC_ACTION     =    'public';
-    const PRIVATE_ACTION    =    'private';
+    const SELF_ACTION    =    'self';
+    const PARENT_ACTION  =    'parent';
+    const PUBLIC_ACTION  =    'public';
+    const PRIVATE_ACTION =    'private';
 
     private $container;
     private $permissionType;
@@ -57,8 +59,26 @@ class CompanyPermission implements MiddlewareInterface {
             try {
                 $permission = $permissionRepository->findOne($actingCompany->id, $routeName);
             } catch (NotFound $e) {
-                throw new NotAllowed();
+                throw new NotAllowed;
             }
+        }
+        
+        if ($this->permissionType === self::SELF_ACTION || $this->permissionType === self::SELF_OR_PARENT_ACTION) {
+            $allowed = false;
+            $targetCompany = $request->getAttribute('targetCompany');
+
+            if ($targetCompany->id == $actingCompany->id) {
+                $allowed = true;
+            } else {
+                // if it is a parent
+                $companyRepository     = $this->container->get('repositoryFactory')->create('Company');
+                $allowed = $companyRepository->isParent($actingCompany, $targetCompany);
+            }
+
+            if (! $allowed) {
+                throw new NotAllowed;
+            }
+
         }
 
         return $next($request, $response);
