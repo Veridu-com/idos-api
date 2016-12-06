6b00dc1bee10caa69ef247924e08f7b8c887cf60
Rafael Andreatta <eu@rafaa.me>
2016-12-06 14:42:33 -0200

    Added functional tests for recommendation endpoint

---
+++ test/Functional/Recommendation/GetOneTest.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ * Copyright (c) 2012-2016 Veridu Ltd <https://veridu.com>
+ * All rights reserved.
+ */
+
+declare(strict_types = 1);
+
+namespace Test\Functional\Recommendation;
+
+use Test\Functional\AbstractFunctional;
+use Test\Functional\Traits;
+
+class GetOneTest extends AbstractFunctional {
+    use Traits\RequiresAuth,
+        Traits\RequiresUserToken,
+        Traits\RejectsCredentialToken,
+        Traits\RejectsIdentityToken;
+
+    protected function setUp() {
+        parent::setUp();
+
+        $this->httpMethod = 'GET';
+        $this->uri        = '/1.0/profiles/f67b96dcf96b49d713a520ce9f54053c/recommendation';
+    }
+
+    public function testSuccess() {
+        $request = $this->createRequest(
+            $this->createEnvironment(
+                [
+                    'HTTP_AUTHORIZATION' => $this->userTokenHeader()
+                ]
+            )
+        );
+        $response = $this->process($request);
+        $this->assertSame(404, $response->getStatusCode());
+
+        $body = json_decode((string) $response->getBody(), true);
+        $this->assertNotEmpty($body);
+        $this->assertFalse($body['status']);
+
+        /*
+         * Validates Response using the Json Schema.
+         */
+        $this->assertTrue(
+            $this->validateSchema(
+                'error.json',
+                json_decode((string) $response->getBody())
+            ),
+            $this->schemaErrors
+        );
+    }
+}
---
+++ test/Functional/Recommendation/UpsertTest.php
@@ -0,0 +1,106 @@
+<?php
+/*
+ * Copyright (c) 2012-2016 Veridu Ltd <https://veridu.com>
+ * All rights reserved.
+ */
+
+declare(strict_types = 1);
+
+namespace Test\Functional\Recommendation;
+
+use Slim\Http\Response;
+use Slim\Http\Uri;
+use Test\Functional\AbstractFunctional;
+use Test\Functional\Traits;
+
+class UpsertTest extends AbstractFunctional {
+    use Traits\RequiresAuth,
+        Traits\RequiresCredentialToken,
+        Traits\RejectsUserToken,
+        Traits\RejectsIdentityToken;
+
+    protected function setUp() {
+        parent::setUp();
+
+        $this->httpMethod = 'PUT';
+        $this->uri        = '/1.0/profiles/f67b96dcf96b49d713a520ce9f54053c/recommendation';
+    }
+
+    public function testInsert() {
+        $environment = $this->createEnvironment(
+            [
+                'HTTP_CONTENT_TYPE'  => 'application/json',
+                'HTTP_AUTHORIZATION' => $this->credentialTokenHeader()
+            ]
+        );
+
+        $request = $this->createRequest(
+            $environment, json_encode([
+                'result'  => true,
+                'reasons' => []
+            ])
+        );
+
+        $response = $this->process($request);
+        $this->assertSame(201, $response->getStatusCode());
+
+        $body = json_decode((string) $response->getBody(), true);
+        $this->assertNotEmpty($body);
+        $this->assertTrue($body['status']);
+        $this->assertSame(true, $body['data']['result']);
+        $this->assertSame([], $body['data']['reasons']);
+
+        /*
+         * Validates Response using the Json Schema.
+         */
+        $this->assertTrue(
+            $this->validateSchema(
+                'recommendation/upsert.json',
+                json_decode((string) $response->getBody())
+            ),
+            $this->schemaErrors
+        );
+    }
+
+    public function testUpdate() {
+        $this->testInsert();
+
+        $environment = $this->createEnvironment(
+            [
+                'HTTP_CONTENT_TYPE'  => 'application/json',
+                'HTTP_AUTHORIZATION' => $this->credentialTokenHeader()
+            ]
+        );
+
+        $request = $this->createRequest(
+            $environment, json_encode([
+                'result'  => false,
+                'reasons' => [
+                    'reason-1',
+                    'reason-2',
+                    'reason-3'
+                ]
+            ])
+        );
+
+        $response = $this->process($request);
+        $this->assertSame(200, $response->getStatusCode());
+
+        $body = json_decode((string) $response->getBody(), true);
+        $this->assertNotEmpty($body);
+        $this->assertTrue($body['status']);
+        $this->assertSame(false, $body['data']['result']);
+        $this->assertSame(['reason-1', 'reason-2', 'reason-3'], $body['data']['reasons']);
+
+        /*
+         * Validates Response using the Json Schema.
+         */
+        $this->assertTrue(
+            $this->validateSchema(
+                'recommendation/upsert.json',
+                json_decode((string) $response->getBody())
+            ),
+            $this->schemaErrors
+        );
+    }
+}
